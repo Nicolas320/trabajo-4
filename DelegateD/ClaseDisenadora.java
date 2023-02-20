3a0035cb7bf90f6a1e5580cf3cb41bf6672c7431
lauravidal22 <lcvidal52@misena.edu.co>
2023-02-20 16:16:01 -0500

    daniel patrones

---
+++ DelegateD/ClaseDisenadora.java
@@ -0,0 +1,7 @@
+package Delegate;
+
+public class ClaseDisenadora implements IDisenadora  {
+public void disenar() {
+	System.out.println("diseñando con la clase diseñara ");
+}
+}
---
+++ DelegateD/PatronDelegate.java
@@ -0,0 +1,12 @@
+package Delegate;
+
+public class PatronDelegate {
+
+	public static void main(String[] args) {
+		
+		 Empleado objEmpleado = new Empleado(new ClaseCodificadora(),new ClaseDisenadora());
+	        objEmpleado.codificar();
+	        objEmpleado.disenar();
+	    }
+	
+}
